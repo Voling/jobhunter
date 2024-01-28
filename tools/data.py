ecd866d5afdd3abe4036cba571ef9af07a175d6e
husainwafaie <117494930+husainwafaie@users.noreply.github.com>
2024-01-27 22:33:20 -0800

    Fallbacks inhanced

--- tools/data.py
+++ tools/data.py
@@ -23,6 +23,7 @@ def geocode(address):
         return response.status, response.reason
 
 
+
 #find exact address of a company given city,state
 def get_company_address(company_name, city):
     host = "maps.googleapis.com"
@@ -56,6 +57,7 @@ def get_surrounding_zip_codes(base_zip, radius=5):
 
     return [zipcode.zipcode for zipcode in nearby_zip_codes]
 
+
 def secondary_search(keyw, loca, rad, lat_input, lng_input):
     host = 'jooble.org'
     key = 'ba0e5c67-fec5-49b3-9f46-5d50a445097b'
@@ -76,6 +78,8 @@ def secondary_search(keyw, loca, rad, lat_input, lng_input):
     s[0] = s[0][s[0].find("[")+1:]
     s[-1] = s[-1][:-3]
     return s
+
+
 
 def search(keyw, loca, rad, lat_input, lng_input):
     host = 'jooble.org'
@@ -108,13 +112,14 @@ def search(keyw, loca, rad, lat_input, lng_input):
         dct = {"Title": "", "Location": "", "Company": "", "Address": "",  
                "Lat": "", "Lng" : "", "Geo_distance" : "", "Real_distance": "", "dummy": 10000}
         #print("Title: ", json.loads(s[i])["title"] + ", " + "Location: " + json.loads(s[i])["location"], end = ", ")
-        dct["Title"] = json.loads(s[i])["title"]
-        dct["Location"] = json.loads(s[i])["location"]
+        json_obj = json.loads(s[i])
+        dct["Title"] = json_obj["title"]
+        dct["Location"] = json_obj["location"]
 
         #print("Company: " + json.loads(s[i])["company"])
-        dct["Company"] = json.loads(s[i])["company"]
-        address = str(get_company_address("".join(json.loads(s[i])["company"].split()), 
-                                                    "".join(str(json.loads(s[i])["location"]).split(", ")[0].split())))
+        dct["Company"] = json_obj["company"]
+        address = str(get_company_address("".join(json_obj["company"].split()), 
+                                                    "".join(str(json_obj["location"]).split(", ")[0].split())))
         #print("The address is: ", address)
         dct["Address"] = address
 
@@ -125,11 +130,12 @@ def search(keyw, loca, rad, lat_input, lng_input):
             dct["Real_distance"] = distance.get_real_distance(coor_to_zip.get_address(lat_input, lng_input),
                                                               coor_to_zip.get_address(dct["Lat"], dct["Lng"]))
             dct["dummy"] = float(dct["Real_distance"][:-3])
+            #l = dct["Address"].split(", ")
         lst.append(dct)
     return sorted(lst, key=lambda x: x["dummy"])
 
-#data = search("AWS", coor_to_zip.get_zip(33.6396288,-117.8435584), 5, 33.6396288,-117.8435584)
-data = search("software engineer", coor_to_zip.get_zip(33.6491, -117.8474), 10, 33.6471, -117.8424)
+
+data = search("software engineer", coor_to_zip.get_zip(33.736696, -118.015753), 10, 33.736696, -118.015753)
 '''
 for d in data:
     print(d)
