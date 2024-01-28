6445f954b2bc24bea2c700eb3bee7e905c15c46a
husainwafaie <117494930+husainwafaie@users.noreply.github.com>
2024-01-27 17:37:26 -0800

    Fixed addresses

--- tools/coor_to_zip.py
+++ tools/coor_to_zip.py
@@ -15,10 +15,32 @@ def get_zip(lat, lng):
 
     if response.status == 200:
         result = json.loads(response.read().decode("utf-8"))
+
         address_components = result.get('results', [])[0].get('address_components', [])
 
         for component in address_components:
             if 'postal_code' in component['types']:
                 return component['long_name']
 
+    return None
+
+def get_address(lat, lng):
+    host = "maps.googleapis.com"
+    base_url = "/maps/api/geocode/json"
+    api_key = 'AIzaSyBCZo8kaQLiNBtcXHuWyLFyCgcWHbgZ4mo'
+
+    params = f"latlng={lat},{lng}&key={api_key}"
+
+    connection = http.client.HTTPSConnection(host)
+    connection.request("GET", f"{base_url}?{params}")
+
+    response = connection.getresponse()
+
+    if response.status == 200:
+        result = json.loads(response.read().decode("utf-8"))
+        formatted_address = result.get('results', [])[0].get('formatted_address', None)
+        connection.close()
+        return formatted_address
+
+    connection.close()
     return None
